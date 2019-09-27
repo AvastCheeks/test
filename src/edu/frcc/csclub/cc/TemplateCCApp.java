f3006f29fb426991bafdf6e259bb709a065335f6
AvastCheeks <seschaak@gmail.com>
2019-09-27 16:22:08 -0600

    Update TemplateCCApp.java
    Added point arrays

--- src/edu/frcc/csclub/cc/TemplateCCApp.java
+++ src/edu/frcc/csclub/cc/TemplateCCApp.java
@@ -5,16 +5,24 @@ import java.awt.Color;
 public class TemplateCCApp extends CCApp {
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 500;
+	private static final int NUM_POINTS = 50;
+
+	private int[] xPoints;
+	private int[] yPoints;
 
 	public TemplateCCApp() {
 		super("Template CCApp", WIDTH, HEIGHT);
 		// WARNING: Do not call getGfx() here.  It will break.
 		// All of your initialization code should go in the begin method.
+		
+		
 	}
 
 	@Override
 	public void begin() {
 		// TODO Initialize your application here.
+		xPoints = new int[NUM_POINTS];
+		yPoints = new int[NUM_POINTS];
 
 		getGfx().setColor(Color.blue);
 		getGfx().fillRect(0, 0, WIDTH, HEIGHT);
